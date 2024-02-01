a60be72c3bfa84a12f6e54ae4652155d9248f9ea
Rowland Oka <rowlandoka@gmail.com>
2024-02-01 06:43:08 +0100

    Initial setup and Project design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,52 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { SessionProvider } from 'next-auth/react';
+import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import darkTheme from '@/theme/darkTheme';
+import lightTheme from '@/theme/lightTheme';
+import Header from '@/pages/components/Header';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const ColorModeContext = React.createContext({
+	toggleColorMode: () => {},
+});
+
+const App = ({ Component, pageProps: { session, ...pageProps } }) => {
+	const [mode, setMode] = React.useState<'light' | 'dark'>('dark');
+	const colorMode = React.useMemo(
+		() => ({
+			toggleColorMode: () => {
+				setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+			},
+		}),
+		[]
+	);
+
+	const darkThemeChosen = React.useMemo(
+		() =>
+			createTheme({
+				...darkTheme,
+			}),
+		[]
+	);
+	const lightThemeChosen = React.useMemo(
+		() =>
+			createTheme({
+				...lightTheme,
+			}),
+		[]
+	);
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider
+				theme={mode === 'dark' ? darkThemeChosen : lightThemeChosen}
+			>
+				<SessionProvider session={session}>
+					<CssBaseline />
+					<Header ColorModeContext={ColorModeContext} />
+					<Component {...pageProps} />
+				</SessionProvider>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
+};
+export default App;
